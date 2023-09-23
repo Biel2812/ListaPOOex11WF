3a64006e1dbd1ee19ce21e2c8aeac1f7692dec32
Biel2812 <130082662+Biel2812@users.noreply.github.com>
2023-09-23 18:37:47 -0300

    Add files via upload

---
+++ PJ011/PJ011/Indice.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PJ011
+{
+    internal class Indice
+    {
+        //atributos
+        private double peso;
+        private double alt;
+        private double ind;
+
+
+
+
+        //metodos 
+        public void setVpeso(double p)
+        {
+            peso = p;
+        }
+
+        public void setValtura(double p)
+        {
+            alt = p;
+        }
+
+
+        public double getVpeso()
+        {
+            return peso;
+        }
+
+        public double getValtura()
+        {
+            return alt;
+        }
+
+        public double getValores()
+        {
+            return ind;
+        }
+
+
+        public void calcularValores()
+
+
+        {
+
+            ind = (peso / (alt * alt));
+
+            if (ind < 20)
+                Console.WriteLine("Abaixo do peso");
+
+            else if (ind <= 20 || ind < 25)
+                Console.WriteLine("Peso ideal");
+
+            else if (ind >= 25)
+                Console.WriteLine("Acima do peso");
+
+
+        }
+    }
+}
+
